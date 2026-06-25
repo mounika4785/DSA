bef4edea2439abc475a2fed52b99aa1734838d17
Revelli Mounika <mounika4785@gmail.com>
2026-06-25 08:36:00 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (57.15%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+       int low = 0;
+       int high = nums.length - 1;
+       while(low < high){
+        int mid = (low + high ) / 2;
+        if(((mid % 2 == 0) && nums[mid+1] == nums[mid]) || ((mid % 2 == 1) && nums[mid-1] == nums[mid])){
+            low = mid +1;
+        }
+        else{
+            high = mid;
+        }
+       }
+       return nums[low];
+    }
+}
