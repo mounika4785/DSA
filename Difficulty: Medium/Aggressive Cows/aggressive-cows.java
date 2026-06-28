ccf33f169bf8c9bda9cad0605e0015493320704c
Revelli Mounika <mounika4785@gmail.com>
2026-06-28 20:34:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public int aggressiveCows(int[] stalls, int k) {
+        Arrays.sort(stalls);
+        int low = 1;
+        int high = stalls[stalls.length - 1] - stalls[0];
+        int result = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canPlace(stalls, k, mid)) {
+                result = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
+    private boolean canPlace(int[] stalls, int k, int dist) {
+        int count = 1;
+        int lastPos = stalls[0];
+        for (int i = 1; i < stalls.length; i++) {
+            if (stalls[i] - lastPos >= dist) {
+                count++;
+                lastPos = stalls[i];
+            }
+            if (count == k) return true;
+        }
+        return false;
+    }
+}
