01fa77ea7b31ed62c93f4558c5a4add69e82604f
Revelli Mounika <mounika4785@gmail.com>
2026-07-21 18:47:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.java
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.java
@@ -1,40 +1,46 @@
 class Solution {
-    private boolean isPossible(int[] arr, int k, int mid) {
-        int studentCount = 1; 
-        int pages = 0;
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > mid) return false;
+    public int countStudents(int[] arr, long maxPages) {
+        int students = 1;
+        long pages = 0;
 
-            if (pages + arr[i] > mid) {
-                studentCount++;   
-                pages = arr[i];   
-                if (studentCount > k) return false; 
+        for (int num : arr) {
+            if (pages + num <= maxPages) {
+                pages += num;
             } else {
-                pages += arr[i];
+                students++;
+                pages = num;
             }
         }
-        return true;
-    }
-    public int findPages(int[] arr, int k) {
-        int n = arr.length;
-        if (k > n) return -1; 
 
-        int low = Integer.MIN_VALUE, high = 0;
-        for (int i = 0; i < n; i++) {
-            high += arr[i];
-            low = Math.max(low, arr[i]); 
+        return students;
+    }
+
+    public int findPages(int[] arr, long k) {
+        int n = arr.length;
+
+        if (k > n) return -1;
+
+        long low = arr[0];
+        long high = 0;
+
+        for (int num : arr) {
+            low = Math.max(low, num);
+            high += num;
         }
-        int result = -1;
+
         while (low <= high) {
-            int mid = (low + high) / 2;
-            if (isPossible(arr, k, mid)) {
-                result = mid;      
+            long mid = low + (high - low) / 2;
+
+            long students = countStudents(arr, mid);
+
+            if (students <= k) {
                 high = mid - 1;
             } else {
-                low = mid + 1;     
+                low = mid + 1;
             }
         }
-        return result;
+
+        return (int)low;
     }
 }
