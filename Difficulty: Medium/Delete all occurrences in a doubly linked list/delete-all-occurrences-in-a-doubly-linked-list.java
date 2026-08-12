0c566d9bc89e69a49725e5fc9834e1f310964657
Revelli Mounika <mounika4785@gmail.com>
2026-08-12 18:26:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Delete all occurrences in a doubly linked list/delete-all-occurrences-in-a-doubly-linked-list.java
@@ -0,0 +1,34 @@
+/* Structure of Doubly Linked List
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        prev = null;
+    }
+}
+*/
+class Solution {
+    static Node deleteAllOccurOfX(Node head, int x) {
+        Node temp = head;
+
+        while (temp != null) {
+            if (temp.data == x) {
+                if (temp.prev != null)
+                    temp.prev.next = temp.next;
+                else
+                    head = temp.next;
+
+                if (temp.next != null)
+                    temp.next.prev = temp.prev;
+            }
+
+            temp = temp.next;
+        }
+
+        return head;
+    }
+}
