48f2c3f706b77d847e521c74c497026bb402001a
Revelli Mounika <mounika4785@gmail.com>
2026-08-15 18:09:43 +0530

    Time: 2 ms (70.21%) | Memory: 44.8 MB (33.21%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,37 +1,21 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> arr = new ArrayList<>();
-        generate(n, "", arr);
+        generate(n, "", 0, 0, arr);
         return arr;
     }
+    public void generate(int n, String s, int open, int close,
+                         List<String> list) {
 
-    public void generate(int n, String s, List<String> list) {
         if (s.length() == 2 * n) {
-
-            if (checkParenthesis(s)) {
-                list.add(s);
-            }
-
+            list.add(s);
             return;
         }
-
-        generate(n, s + "(", list);
-        generate(n, s + ")", list);
-    }
-
-    public boolean checkParenthesis(String s) {
-        int balance = 0;
-
-        for (char c : s.toCharArray()) {
-            if (c == '(')
-                balance++;
-            else
-                balance--;
-
-            if (balance < 0)
-                return false;
+        if (open < n) {
+            generate(n, s + "(", open + 1, close, list);
         }
-
-        return balance == 0;
+        if (close < open) {
+            generate(n, s + ")", open, close + 1, list);
+        }
     }
 }
